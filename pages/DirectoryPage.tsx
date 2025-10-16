74a5d9347a26368ccafbaad339ee8cdc86b537a8
Sriniketh <stponnada@gmail.com>
2025-10-16 20:19:14 +0530

    directory page scaling

--- pages/DirectoryPage.tsx
+++ pages/DirectoryPage.tsx
@@ -175,7 +175,7 @@ const DirectoryPage: React.FC = () => {
   }
 
   return (
-    <div className="w-full min-h-screen">
+    <div>
       <div className="max-w-6xl mx-auto px-4 py-6">
         <div className="mb-6 flex flex-col lg:flex-row items-start lg:items-center justify-between gap-4">
           <div>
