c635c17c3c31c3a6cba04b393e0353a8c1ad8274
Sriniketh <stponnada@gmail.com>
2025-11-23 20:29:21 +0530

    ?

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -80,6 +80,7 @@ const BlogPage: React.FC = () => {
         );
     }
 
+
     if (error || !post) {
         return (
             <div className="flex flex-col items-center justify-center min-h-screen gap-4">
